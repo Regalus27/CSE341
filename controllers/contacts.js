9771b5500fc5d9b6f7d5c4f6e4e456230ed93e36
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2024-09-28 17:05:39 -0500

    W02: Step One

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,8 +1,14 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId; // primary key
+const dbName = 'cse341_w01';
+const collectionName = 'contacts';
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDatabase().db('cse341_w01').collection('contacts').find();
+  const result = await mongodb
+    .getDatabase()
+    .db(dbName)
+    .collection(collectionName)
+    .find();
   result.toArray().then((contacts) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(contacts);
@@ -13,8 +19,8 @@ const getSingle = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDatabase()
-    .db('cse341_w01')
-    .collection('contacts')
+    .db(dbName)
+    .collection(collectionName)
     .find({ _id: userId });
   result.toArray().then((contacts) => {
     res.setHeader('Content-Type', 'application/json');
@@ -22,7 +28,76 @@ const getSingle = async (req, res) => {
   });
 };
 
+// TODO: createUser, updateUser, deleteUser
+const createUser = async (req, res) => {
+  const user = {
+    // pull information from request
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor
+  };
+  // Send request to create user
+  const response = await mongodb
+    .getDatabase()
+    .db(dbName)
+    .collection(collectionName)
+    .insertOne(user);
+  // Track request to create user
+  if (response.acknowledged) {
+    res.status(204).send(); // 204: No Content
+  }
+  else {
+    res.status(500).json(response.error || 'Unknown error occurred while updating the user.');
+  }
+};
+
+const updateUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const user = {
+    // pull information from request
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor
+  };
+  // Send request to create user
+  const response = await mongodb
+    .getDatabase()
+    .db(dbName)
+    .collection(collectionName)
+    .replaceOne({ _id: userId }, user);
+  // Track request to create user
+  if (response.modifiedCount > 0) {
+    res.status(204).send(); // 204: Successfully completed, no response payload body.
+  }
+  else {
+    res.status(500).json(response.error || 'Unknown error occurred while updating the user.');
+    // 500: Server encountered an unexpected condition that prevented it from fulfilling the request.
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // Send request to create user
+  const response = await mongodb
+    .getDatabase()
+    .db(dbName)
+    .collection(collectionName)
+    .deleteOne({ _id: userId });
+  // Track request to create user
+  if (response.deletedCount > 0) {
+    res.status(204).send(); // 204: No Content
+  }
+  else {
+    res.status(500).json(response.error || 'Unknown error occurred while updating the user.');
+  }
+};
+
 module.exports = {
   getAll,
-  getSingle
+  getSingle, 
+  createUser,
+  updateUser,
+  deleteUser
 };
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database.js');
 const app = express();
 
 const port = process.env.PORT || 5959;
 
+// Allows reading body of requests
+app.use(bodyParser.json());
 app.use('/', require('./routes/index.js'));
 
 mongodb.initDb((e) => {
